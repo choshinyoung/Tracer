7b638c29380ccf5654f557a5ef88d857558c43b0
choshinyoung <50575654+choshinyoung@users.noreply.github.com>
2021-07-02 21:47:03 +0900

    fix: crash bug

--- ImNotGoodAtDrawingButTracing/ImNotGoodAtDrawingButTracing/Form1.cs
+++ ImNotGoodAtDrawingButTracing/ImNotGoodAtDrawingButTracing/Form1.cs
@@ -1,44 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ImNotGoodAtDrawingButTracing
 {
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll")]
-        public static extern short GetKeyState(int keyCode);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
-
-        [DllImport("user32.dll")]
-        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
-
-        [DllImport("kernel32.dll")]
-        static extern IntPtr LoadLibrary(string lpFileName);
-
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-
-        const int WH_KEYBOARD_LL = 13;
-        const int WM_KEYDOWN = 0x100;
-        private IntPtr hook = IntPtr.Zero;
-
+        Thread KeyInputThread;
         private bool CapsLockPressed;
 
         public Form1()
         {
-            CapsLockPressed = (GetKeyState((int)Keys.CapsLock) & 0xffff) != 0;
-
             InitializeComponent();
 
             Load += Form1_Load;
@@ -47,14 +23,30 @@ namespace ImNotGoodAtDrawingButTracing
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            IntPtr hInstance = LoadLibrary("User32");
+            CapsLockPressed = IsKeyLocked(Keys.CapsLock);
 
-            hook = SetWindowsHookEx(WH_KEYBOARD_LL, HookProc, hInstance, 0);
+            KeyInputThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(200);
+
+                    bool tmp = IsKeyLocked(Keys.CapsLock);
+
+                    Invoke((MethodInvoker)delegate ()
+                    {
+                        UpdateStyles();
+                    });
+
+                    CapsLockPressed = tmp;
+                }
+            });
+            KeyInputThread.Start();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            UnhookWindowsHookEx(hook);
+            KeyInputThread.Abort();
         }
 
         protected override CreateParams CreateParams
@@ -77,22 +69,6 @@ namespace ImNotGoodAtDrawingButTracing
 
                 return cp;
             }
-        }
-
-        private IntPtr HookProc(int code, IntPtr wParam, IntPtr lParam)
-        {
-            if (code >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-            
-                if (vkCode.ToString() == "20")
-                {
-                    CapsLockPressed = (GetKeyState((int)Keys.CapsLock) & 0xffff) == 0;
-                    UpdateStyles();
-                }
-            }
-
-            return CallNextHookEx(hook, code, (int)wParam, lParam);
         }
 
         private void FindImageButton_Click(object sender, EventArgs e)
